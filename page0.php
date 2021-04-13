cdc6d5fef601bcdec4b01a2461a06a94d1e5348d
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-04-13 02:21:16 -0300

    SESSION VBRANCH

--- page0.php
+++ page0.php
@@ -1,26 +1,2 @@
-<h1> TRY /CATCH </H1><hr>
+<h1> SESSÃO </H1><hr>
 <?php
-// echo 7 /0 ;
-// echo intdiv(7, 0);
-
-//? bloco basico de tratamento de erro
-try {
-    echo intdiv(7,0);
-} catch (\Throwable $th) {
-    echo 'teve um erro aqui<hr>';
-}
-
-//? hierarquia de catchs
-//? e necessario usar varios catchs quando se tem diversas maneiras de tratar um eroo
-try {
-    throw new Exception('um erro muito estranho<hr>');
-    echo intdiv(7,0);
-} catch (DivisionByZeroError $th) {
-    echo 'dividindo por zero<hr>';
-}catch (\Throwable $th) {
-   echo 'erro em:' . $th->getMessage() .'...<hr>';
-}finally{
-    echo 'sempre executado<hr>';
-}
-
-echo 'execução continua<hr>';
--- page2.php
+++ page2.php
@@ -1,23 +1,2 @@
-<h1> Error Handler </H1><hr>
+<h1> SESSÃO </H1><hr>
 <?php
-ini_set('display_errors', 1);
-echo 4 / 0;
-
-error_reporting(E_ERROR);
-error_reporting(E_ALL);
-error_reporting(~E_ALL);
-
-error_reporting(E_ALL);
-echo 4 / 0;
-include 'ARQUI';
-
-function filtrar($errno, $errstring){
-    $text = 'include';
-    return !!stripos("$errstring", $text);
-}
-set_error_handler('filtrarMensagem', E_WARNING);
-
-echo 4 / 0;
-include 'ARQUI';
-
-restore_error_handler();
