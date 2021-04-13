c724616e82d4a97e55aef40d3b3b5c84e2929313
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-04-13 01:46:14 -0300

    uopdtae

--- page0.php
+++ page0.php
@@ -3,12 +3,15 @@
 // echo 7 /0 ;
 // echo intdiv(7, 0);
 
+//? bloco basico de tratamento de erro
 try {
     echo intdiv(7,0);
 } catch (\Throwable $th) {
     echo 'teve um erro aqui<hr>';
 }
 
+//? hierarquia de catchs
+//? e necessario usar varios catchs quando se tem diversas maneiras de tratar um eroo
 try {
     throw new Exception('um erro muito estranho<hr>');
     echo intdiv(7,0);
