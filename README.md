dc2ef54fd9cb34ae5ff2aeba1327ed9e2a916f95
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-04-13 23:09:00 -0300

    sessao

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 #### Sessão
 
-    - ...
+    - antes de começar a usar sessão é necessario usar o metodo session_start
+        - inicializa
+        - permite usar
+    - a sessão é um array
+    - para destruir uma sessão se usa session_destroy()
+
+    - como gerenciar sessão
+    - cada sessão tem um ID
+    - é importante regenerar esse id sempre
+    para nao roubarem
+    
--- page0.php
+++ page0.php
@@ -1,2 +1,21 @@
 <h1> SESSÃO </H1><hr>
 <?php
+
+//? session_id('');
+session_start();
+echo session_id(); //! get or set
+
+//? salvando endereço de memoria da sessai
+$contador = &$_SESSION['contador'];
+$contador = $contador ? $contador + 1 : 1;
+echo '<br>' . $_SESSION['contador'];
+
+if($_SESSION['contador'] % 5 === 0){
+    //? É importante que regenere baseado em alguma regra de negocio. segunrança;
+    session_regenerate_id();
+}
+
+if($_SESSION['contador'] >= 15){
+    //? destroi a sessao
+    session_destroy();
+}
