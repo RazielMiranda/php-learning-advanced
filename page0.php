cae81c16b79cf5ce027f4a75d3387b06d7b599c1
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-04-23 01:45:59 -0300

    BRANCH REFRESH

--- page0.php
+++ page0.php
@@ -1,21 +1,2 @@
-<h1> SESSÃO </H1><hr>
+<h1> FORMS </H1><hr>
 <?php
-
-//? session_id('');
-session_start();
-echo session_id(); //! get or set
-
-//? salvando endereço de memoria da sessao
-$contador = &$_SESSION['contador'];
-$contador = $contador ? $contador + 1 : 1;
-echo '<br>' . $_SESSION['contador'];
-
-if($_SESSION['contador'] % 5 === 0){
-    //? É importante que regenere baseado em alguma regra de negocio. segurança;
-    session_regenerate_id();
-}
-
-if($_SESSION['contador'] >= 15){
-    //? destroi a sessao
-    session_destroy();
-}
--- page2.php
+++ page2.php
@@ -1,2 +1,2 @@
-<h1> SESSÃO </H1><hr>
+<h1> FORMS </H1><hr>
 <?php
