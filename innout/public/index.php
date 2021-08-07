ef7cce698a054d77032f1a648483c8c07002f720
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-08-07 20:28:19 -0300

    get result

--- innout/public/index.php
+++ innout/public/index.php
@@ -2,4 +2,10 @@
 
 require_once(dirname(__FILE__ , 2)) . '/src/config/database.php';
 
-Database::getConnection();
+$sql = "SELECT * FROM USERS";
+$result = Database::getResultFromQuery($sql);
+
+while ($row = $result->fetch_assoc()) {
+    print_r($row);
+    echo "<br/>";
+}
