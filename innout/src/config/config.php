1255d8905711fa3582106fb0c4e0022f31f321ee
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-08-22 18:08:50 -0300

    començando controllers

--- innout/src/config/config.php
+++ innout/src/config/config.php
@@ -5,7 +5,8 @@ setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
 // Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
-define('VIEWS_PATH', realpath(dirname(__FILE__) . '/../views'));
+define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
+define('CONTROLLER_PATH', realpath(dirname(__FILE__) . '/../controllers'));
 
 // Arquivos importates
 require_once(realpath(dirname(__FILE__) . '/database.php'));
