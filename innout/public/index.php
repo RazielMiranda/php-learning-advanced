6d6df199850b453102ee314f8d56ae3fcc3ec5d9
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-08-22 16:11:47 -0300

    tela de login front-end

--- innout/public/index.php
+++ innout/public/index.php
@@ -4,10 +4,11 @@
 require_once (dirname(__FILE__, 2)) . '/src/config/config.php';
 
 //Incluindo classe
-require_once (dirname(__FILE__, 2)) . '/src/models/User.php';
+// require_once (dirname(__FILE__, 2)) . '/src/models/User.php';
+require_once (dirname(__FILE__, 2)) . '/src/views/login.php';
 
 //Passando dados para a model User
-$user = new User(['name' => 'Raziel', 'email' => 'razielx3@live.com']);
+// $user = new User(['name' => 'Raziel', 'email' => 'razielx3@live.com']);
 
 // //Apenas chamando o objeto
 // var_dump($user);
@@ -18,6 +19,6 @@ $user = new User(['name' => 'Raziel', 'email' => 'razielx3@live.com']);
 // //Usando metodo magico get
 // var_dump($user->name);
 
-print_r(User::get(['id' => 1] , 'name, email'));
+// print_r(User::get(['id' => 1] , 'name, email'));
 
 // var_dump(User::get([], 'name'));
---
+++ innout/src/views/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/comum.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/icofont.min.css">
+    <link rel="stylesheet" href="assets/css/login.css">
+    <title>In 'n Out</title>
+</head>
+<body>
+    <form class="form-login" action="#" method="post">
+        <div class="card login-card">
+            <div class="card-header">
+                <i class="icofont-travelling"></i>
+                <span class="font-weight-light mr-2">In </span>
+                <span class="font-weight-bold mx-2">N' </span>
+                <span class="font-weight-light ml-2">Out </span>
+                <i class="icofont-runner-alt-1"></i>
+            </div>
+            <div class="card-body">
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email"
+                        class="form-control"
+                        placeholder="Informe o e-mail" autofocus>
+                </div>
+                <div class="form-group">
+                    <label for="senha">Senha</label>
+                    <input type="senha" name="senha" id="senha"
+                        class="form-control"
+                        placeholder="Informe a senha">
+                </div>
+            </div>
+            <div class="card-footer">
+                <button class="btn btn-lg btn-primary">Entrar</button>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
