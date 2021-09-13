8de2c328f199f3cdf9e7ac4f920dae140a03a459
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-09-12 21:33:13 -0300

    main content css

--- innout/src/views/day_records.php
+++ innout/src/views/day_records.php
@@ -1,62 +1,38 @@
 <main class="content">
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
-    <h1>Olá mundo</h1>
+    
+    <div class="content-title mb-4">
+        <i class="icofont-check-alt mr-2"></i>
+        <div>
+            <h1>Registrar ponto</h1>
+            <h2>mantenha seu ponto consistente!</h2>
+        </div>
+    </div>
+
+    <div class="card">
+
+        <div class="card-header">
+            <h3>10 de janeiro de 2025</h3>
+            <p class="mb-0">Os batimentos efetuados hoje</p>
+        </div>
+
+        <div class="card-body">
+            <div class="d-flex m-5 justify-content-around">
+                <span>Entrada 1: ----</span>
+                <span>Saida 1: ----</span>
+            </div>
+            <div class="d-flex m-5 justify-content-around">
+                <span>Entrada 2: ----</span>
+                <span>Saida 2: ----</span>
+            </div>
+        </div>
+
+        <div class="card-footer d-flex justify-content-center">
+            <a href="?" class="btn btn-success btn-lg">
+                <i class="icofont-check mr-1"></i>
+                Bater o ponto
+            </a>
+        </div>
+
+    </div>
 
 </main>
