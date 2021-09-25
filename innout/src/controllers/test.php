58a029a3d1a6c3ba218656143b339603626ee0af
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-09-25 18:11:33 -0300

    calculando as horas de trabalho

--- innout/src/controllers/test.php
+++ innout/src/controllers/test.php
@@ -2,11 +2,7 @@
 
 echo 'teste!';
 
+loadModel('WorkingHours');
 
-$i1 = DateInterval::createFromDateString('9 hours');
-$i2 = DateInterval::createFromDateString('12 hours');
-var_dump(getDateFromInterval(sumIntervals($i1, $i2)));
-
-$r1 = DateInterval::createFromDateString('9 hours');
-$r2 = DateInterval::createFromDateString('7 hours');
-var_dump(getDateFromInterval(subtractIntervals($r1, $r2)));
+$wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
+var_dump($wh->getWorkedInterval()->format("%H:%i:%s"));
