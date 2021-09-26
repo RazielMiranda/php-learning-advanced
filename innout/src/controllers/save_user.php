5a5a6727263d56801e81ebd7437331a3997e455a
Raziel Rodrigues <raziel@upoutsourcing.com>
2021-09-26 16:25:48 -0300

    validando usuarios

---
+++ innout/src/controllers/save_user.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+requireValidSession();
+
+try {
+    $newUser = new User($_POST);
+    $newUser->insert();
+    addSuccesMsg('Usuario cadastrado com sucesso!');
+    $_POST = [];
+} catch (Exception $e) {
+    $exception = $e;
+}
+
+loadTemplateView('save_user', ['exception' => $exception]);
